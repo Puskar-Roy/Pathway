c586d08c7ef4477211fbc76383dc4d3da0b98ca5
Puskar-Roy <puskarroy600@gmail.com>
2023-12-12 12:44:00 +0530

    feat : Deployment

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "https://pathway.onrender.com";
+export const host = "https://pathwayy-backend.vercel.app";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
